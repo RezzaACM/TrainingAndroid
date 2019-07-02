6795147905076ae73ffe5b8f5c6d73cc99330923
RezzaACM <rezza1207@gmail.com>
2019-07-02 14:16:27 +0700

    Update data karyawan

--- app/src/main/java/com/example/training2/ListAdapter.java
+++ app/src/main/java/com/example/training2/ListAdapter.java
@@ -6,6 +6,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -51,6 +54,23 @@ public class ListAdapter extends BaseAdapter {
             case 1:
                 if(convertView == null)
                     vi = inflater.inflate(R.layout.employee_lv, null);
+
+                TextView employeeName = (TextView) vi.findViewById(R.id.namaKaryawan);
+                TextView employeeNumber = (TextView) vi.findViewById(R.id.number);
+                TextView employeeAddress = (TextView) vi.findViewById(R.id.address);
+                TextView employeeGender = (TextView) vi.findViewById(R.id.gender);
+
+                HashMap<String, String> empList = new HashMap<>();
+
+                empList = data.get(position);
+
+                employeeName.setText(empList.get("employee_name"));
+                employeeName.setText(empList.get("nomor_induk_pegawai"));
+                employeeName.setText(empList.get("address"));
+                employeeName.setText(empList.get("gender"));
+
+                break;
+
         }
         return null;
     }
